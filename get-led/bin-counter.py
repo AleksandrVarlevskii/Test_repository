463a7aacfcf2902dec4f205c70b7691cc62d27ec
Малина <varlevskii@phystech.edu>
2025-09-16 12:08:35 +0300

    Добалвен недоделанный скрипт двоичного счетчика. Проблема с включением светодиодов. Исправить

---
+++ get-led/bin-counter.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup([9, 10], GPIO.IN)
+GPIO.output(leds, 0)
+num =0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+while True:
+    if GPIO.input(9):
+        num+=1
+        print(num, dec2bin(num))
+        time.sleep(0.2)
+    if GPIO.input(10):
+        num-=1
+        print(num, dec2bin(num))
+        time.sleep(0.2)
+    for i in range(7) :
+        GPIO.output(leds[i], dec2bin(num)[i])     
