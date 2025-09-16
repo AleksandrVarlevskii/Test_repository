f89b6cb9f541e37b5abc0c236e0d295b1d245559
Малина <varlevskii@phystech.edu>
2025-09-16 10:34:34 +0300

    Добавленна папка для скриптов, необходимых для работы малинки

---
+++ get-led/digital-blink.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+state = 0
+period =1.0
+while True :
+    GPIO.output(26, state)
+    if state == 0:
+        state =2
+    state -=1    
+    time.sleep(period)
