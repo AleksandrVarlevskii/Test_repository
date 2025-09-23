62ea22fa65ad95b329955ec2697a277c76b87f01
Малина <varlevskii@phystech.edu>
2025-09-23 09:45:23 +0300

    Исправлены проблемы с включением светодиодов в скрипте двоичного счетчика. Добалвенна защита от чисел меньших нуля и больших максимума. Подтверждена роботоспособность.

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -11,13 +11,13 @@ def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
 while True:
-    if GPIO.input(9):
+    if GPIO.input(9) and num<255:
         num+=1
         print(num, dec2bin(num))
         time.sleep(0.2)
-    if GPIO.input(10):
+    if GPIO.input(10) and num>0:
         num-=1
         print(num, dec2bin(num))
-        time.sleep(0.2)
-    for i in range(7) :
+        time.sleep(0.2)   
+    for i in range(0,8) :
         GPIO.output(leds[i], dec2bin(num)[i])     
